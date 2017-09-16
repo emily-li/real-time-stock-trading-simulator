03b48210af298094637e22661ec397104a7d90c2
emily-li <emily.li@live.co.uk>
2017-09-16 22:02:57 +0100

    Making StockGenerationService component

--- stock-generation/src/main/java/com/liemily/stock/generation/StockGenerationService.java
+++ stock-generation/src/main/java/com/liemily/stock/generation/StockGenerationService.java
@@ -1,5 +1,10 @@
 package com.liemily.stock.generation;
 
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
 public class StockGenerationService {
     void generateStock(String stockId) {
     }
