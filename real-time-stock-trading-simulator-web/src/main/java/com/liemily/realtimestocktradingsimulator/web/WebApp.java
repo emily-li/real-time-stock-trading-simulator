388a06c42fd4bd31c56be848cef1624607fd52f5
emily-li <emily.li@live.co.uk>
2017-09-16 21:13:06 +0100

    Adding WebApp class

---
+++ real-time-stock-trading-simulator-web/src/main/java/com/liemily/realtimestocktradingsimulator/web/WebApp.java
@@ -0,0 +1,14 @@
+package com.liemily.realtimestocktradingsimulator.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Emily Li on 16/09/2017.
+ */
+@SpringBootApplication
+public class WebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
+}
