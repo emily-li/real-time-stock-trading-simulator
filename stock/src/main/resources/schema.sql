93780ab7309f46357ee03900b302440fcdbd6b39
emily-li <emily.li@live.co.uk>
2017-09-17 01:45:41 +0100

    Adding stock view table

--- stock/src/main/resources/schema.sql
+++ stock/src/main/resources/schema.sql
@@ -1,7 +1,13 @@
 DROP TABLE IF EXISTS `stock`;
+DROP TABLE IF EXISTS `stock_view`;
 
-CREATE TABLE IF NOT EXISTS `stock` (
-    `symbol`        VARCHAR(50)     NOT NULL PRIMARY KEY,
-    `value`         DECIMAL(65,2)   NOT NULL,
-    `volume`        INT             NOT NULL
+CREATE TABLE IF NOT EXISTS stock (
+    symbol        VARCHAR(50)     NOT NULL  PRIMARY KEY,
+    value         DECIMAL(65,2)   NOT NULL,
+    volume        INT             NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS stock_view (
+    symbol        VARCHAR(50)     NOT NULL  PRIMARY KEY,
+    open_value    DECIMAL(65,2)   NOT NULL
 );
