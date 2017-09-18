91b22dc2c4fcd1c0e04474ee79492001afe57c2d
emily-li <emily.li@live.co.uk>
2017-09-18 23:00:44 +0100

    Adding Spring Boot app

---
+++ stock/src/main/java/com/liemily/stock/StockApp.java
@@ -0,0 +1,12 @@
+package com.liemily.stock;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by Emily Li on 18/09/2017.
+ */
+public class StockApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApp.class, args);
+    }
+}
