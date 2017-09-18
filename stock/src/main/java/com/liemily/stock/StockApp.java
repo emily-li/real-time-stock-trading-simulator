d6074c57473398b652cfde4153780fb622517929
emily-li <emily.li@live.co.uk>
2017-09-18 23:03:25 +0100

    Adding missing SpringBootApplication annotation

--- stock/src/main/java/com/liemily/stock/StockApp.java
+++ stock/src/main/java/com/liemily/stock/StockApp.java
@@ -1,10 +1,12 @@
 package com.liemily.stock;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by Emily Li on 18/09/2017.
  */
+@SpringBootApplication
 public class StockApp {
     public static void main(String[] args) {
         SpringApplication.run(StockApp.class, args);
