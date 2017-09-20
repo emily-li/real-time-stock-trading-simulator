8c7db17f6cc4768863dc401d4b8de0a69f5e2e73
emily-li <emily.li@live.co.uk>
2017-09-21 00:54:43 +0100

    Adding component scan to StockConfig

--- stock/src/main/java/com/liemily/stock/config/StockConfig.java
+++ stock/src/main/java/com/liemily/stock/config/StockConfig.java
@@ -1,6 +1,7 @@
 package com.liemily.stock.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * Created by Emily Li on 20/09/2017.
  */
 @Configuration
+@ComponentScan("com.liemily.stock")
 @EnableJpaRepositories("com.liemily.stock")
 @EntityScan("com.liemily.stock")
 @Lazy
