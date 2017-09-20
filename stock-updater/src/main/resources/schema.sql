a440914cc057321ccd1769db517165163e85baa6
emily-li <emily.li@live.co.uk>
2017-09-21 00:50:28 +0100

    Refactoring stock updater to its own module

--- stock-updater/src/main/resources/schema.sql
+++ stock-updater/src/main/resources/schema.sql
@@ -1,23 +1,15 @@
-DROP TABLE IF EXISTS stock;
-DROP TABLE IF EXISTS stock_as_of_details;
-DROP VIEW  IF EXISTS stock_view;
-DROP EVENT IF EXISTS stock_as_of_details_open_update;
-DROP EVENT IF EXISTS stock_as_of_details_close_update;
-
-SET GLOBAL event_scheduler = ON;
-
-CREATE TABLE stock (
+CREATE TABLE IF NOT EXISTS stock (
     symbol        VARCHAR(50)     NOT NULL  PRIMARY KEY,
     value         DECIMAL(65,2)   NOT NULL,
     volume        INT             NOT NULL
 );
 
-CREATE TABLE stock_as_of_details (
+CREATE TABLE IF NOT EXISTS stock_as_of_details (
     symbol        VARCHAR(50)     NOT NULL  PRIMARY KEY,
     open          DECIMAL(65,2),
     close         DECIMAL(65,2)
 );
 
-CREATE VIEW stock_view AS
+CREATE OR REPLACE VIEW stock_view AS
     SELECT stock.symbol, (value - open) AS gains
     FROM stock LEFT JOIN stock_as_of_details ON stock.symbol=stock_as_of_details.symbol;
