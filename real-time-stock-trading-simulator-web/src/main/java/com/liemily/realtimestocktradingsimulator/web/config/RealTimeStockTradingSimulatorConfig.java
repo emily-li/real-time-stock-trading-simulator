d72a659e35dd62326cbeadd52a73fdf08f668122
emily-li <emily.li@live.co.uk>
2017-09-21 21:10:35 +0100

    Adding UserConfig

--- real-time-stock-trading-simulator-web/src/main/java/com/liemily/realtimestocktradingsimulator/web/config/RealTimeStockTradingSimulatorConfig.java
+++ real-time-stock-trading-simulator-web/src/main/java/com/liemily/realtimestocktradingsimulator/web/config/RealTimeStockTradingSimulatorConfig.java
@@ -1,6 +1,7 @@
 package com.liemily.realtimestocktradingsimulator.web.config;
 
 import com.liemily.stock.config.StockConfig;
+import com.liemily.user.config.UserConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -8,6 +9,6 @@ import org.springframework.context.annotation.Import;
  * Created by Emily Li on 20/09/2017.
  */
 @Configuration
-@Import(StockConfig.class)
+@Import({StockConfig.class, UserConfig.class})
 public class RealTimeStockTradingSimulatorConfig {
 }
