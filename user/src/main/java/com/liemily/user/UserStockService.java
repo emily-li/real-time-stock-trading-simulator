172c0b8b1b83a55f9b0bd8a12a13590db40dba40
emily-li <emily.li@live.co.uk>
2017-09-21 21:55:22 +0100

    Autowiring UserStockRepository

--- user/src/main/java/com/liemily/user/UserStockService.java
+++ user/src/main/java/com/liemily/user/UserStockService.java
@@ -2,6 +2,7 @@ package com.liemily.user;
 
 import com.liemily.user.domain.UserStock;
 import com.liemily.user.repository.UserStockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,11 @@ import java.util.List;
 @Lazy
 public class UserStockService {
     private UserStockRepository userStockRepository;
+
+    @Autowired
+    public UserStockService(UserStockRepository userStockRepository) {
+        this.userStockRepository = userStockRepository;
+    }
 
     public List<UserStock> findByUsername(String username) {
         return userStockRepository.findByUsername(username);
