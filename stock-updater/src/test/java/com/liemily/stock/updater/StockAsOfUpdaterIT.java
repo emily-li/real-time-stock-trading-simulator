0101f865d4fdda3f9c79c66d3b0cf3b67ddc5484
emily-li <emily.li@live.co.uk>
2017-09-22 23:21:29 +0100

    Making application.properties common resource to the project

--- stock-updater/src/test/java/com/liemily/stock/updater/StockAsOfUpdaterIT.java
+++ stock-updater/src/test/java/com/liemily/stock/updater/StockAsOfUpdaterIT.java
@@ -61,7 +61,7 @@ public class StockAsOfUpdaterIT {
     /**
      * S.S06 - Stocks should have field 'Open' with field 'Value' as of 0800
      *
-     * @see StockAsOfUpdaterTest for mocking of the time
+     * @see StockAsOfUpdaterOpenCloseIT for mocking of the time
      */
     @Test
     public void testStocksOpenUpdatesAtTime() throws Exception {
@@ -74,7 +74,7 @@ public class StockAsOfUpdaterIT {
     /**
      * S.S07 - Stocks should have field ‘Close’ with field ‘Value’ as of 1630
      *
-     * @see StockAsOfUpdaterTest for mocking of the time
+     * @see StockAsOfUpdaterOpenCloseIT for mocking of the time
      */
     @Test
     public void testStocksCloseUpdatesAtTime() throws Exception {
