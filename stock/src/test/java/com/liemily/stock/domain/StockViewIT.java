b6d9e5c857cb745235a59627910620c70fd37e79
emily-li <emily.li@live.co.uk>
2017-09-22 14:58:18 +0100

    Adding test to verify StockView details

--- stock/src/test/java/com/liemily/stock/domain/StockViewIT.java
+++ stock/src/test/java/com/liemily/stock/domain/StockViewIT.java
@@ -1,18 +1,56 @@
 package com.liemily.stock.domain;
 
+import com.liemily.stock.repository.StockRepository;
+import com.liemily.stock.repository.StockViewRepository;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Emily Li on 22/09/2017.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class StockViewIT {
+    @Autowired
+    private StockViewRepository stockViewRepository;
+    @Autowired
+    private StockRepository stockRepository;
+
+    private StockView stockView;
+
+    @Before
+    public void setup() {
+        String symbol = UUID.randomUUID().toString();
+        Stock stock = new Stock(symbol, new BigDecimal(1), 1);
+        stockRepository.save(stock);
+        stockView = stockViewRepository.findOne(symbol);
+    }
 
     /**
      * C.S06 Stock data should present the following fields as a number: Gains, Value, Volume, Open, Close
      */
     @Test
     public void testStockDataNumberFields() {
+        Collection<BigDecimal> values = new ArrayList<>();
+        values.add(stockView.getGains());
+        values.add(stockView.getValue());
+        values.add(stockView.getOpenValue());
+        values.add(stockView.getCloseValue());
 
+        values.forEach(Assert::assertNotNull);
+        assertEquals(1, stockView.getVolume());
     }
 
     /**
