3d428b946376cc76fdba51b486ee061f64a3ab13
emily-li <emily.li@live.co.uk>
2017-09-23 14:08:13 +0100

    Adding StockModulationApp to launch StockModulationService

---
+++ stock-modulation/src/main/java/com/liemily/stock/modulation/StockModulationApp.java
@@ -0,0 +1,14 @@
+package com.liemily.stock.modulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Emily Li on 23/09/2017.
+ */
+@SpringBootApplication
+public class StockModulationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StockModulationApp.class, args);
+    }
+}
