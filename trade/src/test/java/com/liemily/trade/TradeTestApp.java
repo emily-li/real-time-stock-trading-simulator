460362d574aaf3a5b3b6a370f799315b72bfaabd
emily-li <emily.li@live.co.uk>
2017-10-01 16:40:58 +0100

    Removing unwanted suppression

--- trade/src/test/java/com/liemily/trade/TradeTestApp.java
+++ trade/src/test/java/com/liemily/trade/TradeTestApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Created by Emily Li on 22/09/2017.
  */
-@SuppressWarnings("WeakerAccess")
 @SpringBootApplication
 public class TradeTestApp {
 }
