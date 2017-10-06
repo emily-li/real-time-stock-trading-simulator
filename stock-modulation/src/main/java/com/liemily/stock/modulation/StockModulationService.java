cf5971c4ab0a8f7d71c0772912266e23e2d8d8e7
emily-li <emily.li@live.co.uk>
2017-10-06 21:14:58 +0100

    Removing @Lazy

--- stock-modulation/src/main/java/com/liemily/stock/modulation/StockModulationService.java
+++ stock-modulation/src/main/java/com/liemily/stock/modulation/StockModulationService.java
@@ -1,14 +1,17 @@
 package com.liemily.stock.modulation;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Long running StockModulationService that schedules an update job for modulation
+ * Note that this should not be marked as @Lazy
+ * This service keeps the app running with its executor service
+ */
 @Service
-@Lazy
 class StockModulationService {
     private ScheduledExecutorService scheduledExecutorService;
     private StockModulator stockModulator;
