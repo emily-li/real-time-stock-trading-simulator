0226f1dbbc60af3c4436269fc362829ccc618cd0
emily-li <emily.li@live.co.uk>
2017-10-06 21:14:48 +0100

    Refactoring

--- real-time-stock-trading-simulator-web/src/main/java/com/liemily/realtimestocktradingsimulator/web/WebApp.java
+++ real-time-stock-trading-simulator-web/src/main/java/com/liemily/realtimestocktradingsimulator/web/WebApp.java
@@ -1,14 +1,12 @@
 package com.liemily.realtimestocktradingsimulator.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by Emily Li on 16/09/2017.
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 class WebApp {
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
