bccefe0f2a051244f2d8d7450f651606f4ce3f41
emily-li <emily.li@live.co.uk>
2017-10-07 19:25:23 +0100

    Adding logging

--- stock-modulation/src/main/java/com/liemily/stock/modulation/StockModulator.java
+++ stock-modulation/src/main/java/com/liemily/stock/modulation/StockModulator.java
@@ -2,10 +2,13 @@ package com.liemily.stock.modulation;
 
 import com.liemily.stock.domain.Stock;
 import com.liemily.stock.service.StockService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
 import java.util.Collection;
 
@@ -15,6 +18,7 @@ import java.util.Collection;
 @Component
 @Lazy
 class StockModulator implements Runnable {
+    private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
     private StockService stockService;
     private StockModulationRandomiser stockModulationRandomiser;
 
@@ -31,7 +35,10 @@ class StockModulator implements Runnable {
             double newValue = modulateValue(stock.getValue().doubleValue());
             stock.setValue(BigDecimal.valueOf(newValue));
         }
-        stockService.save(stocks);
+        if (!stocks.isEmpty()) {
+            stockService.save(stocks);
+            logger.info("Updated " + stocks.size() + " stocks");
+        }
     }
 
     private double modulateValue(double initialValue) {
